a1a443ccf1d08e7029d423fbdf90586a4f46a2f2
ys <shuo-yan20@mails.tsinghua.edu.cn>
2026-06-25 13:51:04 +0800

    final625

--- examples/copd_trainer/run_alfworld_copd_guide.sh
+++ examples/copd_trainer/run_alfworld_copd_guide.sh
@@ -15,7 +15,7 @@ GROUP_SIZE=8
 NUM_CPUS_PER_ENV_WORKER=0.1
 
 # COPD advantage and teacher/OPD signal schedule.
-COPD_MODE=mean_norm
+COPD_MODE=mean_std_norm
 COPD_STEP_ADV_W=0.0
 COPD_EPISODE_HINT_TEACHER_ADV_W=${COPD_EPISODE_HINT_TEACHER_ADV_W:-0.001}
 COPD_STEP_HINT_TEACHER_ADV_W=${COPD_STEP_HINT_TEACHER_ADV_W:-0.001}
@@ -36,7 +36,7 @@ COPD_ANALYSIS_MAX_STEP_HINTS_PER_TRAJ=${COPD_ANALYSIS_MAX_STEP_HINTS_PER_TRAJ:-5
 
 # Experiment naming and output location.
 PROJECT_NAME=agentic_alfworld
-EXPERIMENT_NAME=${EXPERIMENT_NAME:-copd-grpo_qwen2.5_3b_alfworld_llm-5_episode-step-hint-v3_opd-adv-0.001_exp2-ds-v32-50}
+EXPERIMENT_NAME=${EXPERIMENT_NAME:-copd-grpo_qwen2.5_3b_alfworld_llm-5_episode-step-hint-v3_opd-adv-0.001_exp2-ds-v32-50-mean-std-norm}
 DEFAULT_LOCAL_DIR=${DEFAULT_LOCAL_DIR:-$MODELS_ROOT/ckpt/$EXPERIMENT_NAME}
 
 # Prompt observation history.
